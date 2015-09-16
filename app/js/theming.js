79741ed99cb62086dbb5f83f72a2a162141b0cdd
GarfieldxD <marcohecktor@googlemail.com>
2015-09-16 09:39:22 +0200

    Added backbone files and configuration

---
+++ app/js/theming.js
@@ -0,0 +1,21 @@
+(function () {    
+  'use strict';
+  
+    angular.module('starterApp')
+        .config(function ($mdThemingProvider, $mdIconProvider) {
+
+            $mdIconProvider
+                .defaultIconSet("./content/svg/avatars.svg", 128)
+                .icon("menu", "./content/svg/menu.svg", 24)
+                .icon("share", "./content/svg/share.svg", 24)
+                .icon("google_plus", "./content/svg/google_plus.svg", 512)
+                .icon("hangouts", "./content/svg/hangouts.svg", 512)
+                .icon("twitter", "./content/svg/twitter.svg", 512)
+                .icon("phone", "./content/svg/phone.svg", 512);
+
+            $mdThemingProvider.theme('default')
+                .primaryPalette('brown')
+                .accentPalette('red');
+
+        });
+})();
