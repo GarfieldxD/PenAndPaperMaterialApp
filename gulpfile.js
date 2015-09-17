41a5d9dfbae53efd8ab23a2f75c6a3beb822845c
GarfieldxD <marcohecktor@googlemail.com>
2015-09-17 08:41:06 +0200

    Refactored Gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -9,28 +9,17 @@ var plugins = require("gulp-load-plugins")({
   pattern: ['gulp-*', 'gulp.*'],
   replaceString: /\bgulp[\-.]/
 });
+
+
 // Concatenate & Minify JS
 gulp.task('scripts', function () {
   return gulp.src(src + 'js/**/*.js')
     .pipe(plugins.concat('app.js'))
     .pipe(plugins.rename({ suffix: '.min' }))
-    //.pipe(plugins.uglify())
+    .pipe(plugins.uglify())
     .pipe(gulp.dest(dest + 'js'));
 });
-gulp.task('images', function () {
-  return gulp.src(src + 'images/**/*')
-    .pipe(plugins.cache(plugins.imagemin({ optimizationLevel: 5, progressive: true, interlaced: true })))
-    .pipe(gulp.dest(dest + 'img'));
-});
-// Watch for changes in files
-gulp.task('watch', function () {
-  // Watch .js files
-  gulp.watch(src + 'js/*.js', ['scripts']);
-  // Watch image files
-  gulp.watch(src + 'images/**/*', ['images']);
-  // Watch whole dir
-  gulp.watch(src + '/**/*.*', ['build']);
-}); 
+
 //webserver
 gulp.task('webserver', function () {
   gulp.src(src)
@@ -40,7 +29,7 @@ gulp.task('webserver', function () {
     }));
 });
 
-//webserver
+//webserver-build
 gulp.task('webserver-build', function () {
   gulp.src(dest)
     .pipe(plugins.serverLivereload({
@@ -51,10 +40,20 @@ gulp.task('webserver-build', function () {
 
 gulp.task('vendor-scripts', function () {
   gulp.src(wiredep().js)
-    .pipe(gulp.dest(dest + 'vendor'));
+    .pipe(gulp.dest(src + 'vendor'));
 });
 
 gulp.task('vendor-css', function () {
+  gulp.src(wiredep().css)
+    .pipe(gulp.dest(src + 'vendor'));
+});
+
+gulp.task('vendor-scripts-build', function () {
+  gulp.src(wiredep().js)
+    .pipe(gulp.dest(dest + 'vendor'));
+});
+
+gulp.task('vendor-css-build', function () {
   gulp.src(wiredep().css)
     .pipe(gulp.dest(dest + 'vendor'));
 });
@@ -64,8 +63,8 @@ gulp.task('my-files', function () {
     .pipe(gulp.dest('./build/modules'));
   gulp.src(['app/content/**/*.*'])
     .pipe(gulp.dest('./build/content'));
-  //gulp.src(['app/js/**/*.js'])
-  //  .pipe(gulp.dest('./build/js'));
+  gulp.src(['app/layout.html'])
+    .pipe(gulp.dest('./build/'));
 
 });
 
@@ -111,7 +110,48 @@ gulp.task('index', function () {
     .pipe(gulp.dest(dest));
 });
 
+gulp.task('index-dev', function () {
+  gulp.src('app/index.html')
+    .pipe(wiredep.stream({
+      fileTypes: {
+        html: {
+          replace: {
+            js: function (filePath) {
+              return '<script src="' + 'vendor/' + filePath.split('/').pop() + '"></script>';
+            },
+            css: function (filePath) {
+              return '<link rel="stylesheet" href="' + 'vendor/' + filePath.split('/').pop() + '"/>';
+            }
+          }
+        }
+      }
+    }))
+    .pipe(plugins.inject(
+      gulp.src([src + 'content/**/*.css'], { read: false }), {
+        addRootSlash: false,
+        transform: function (filePath, file, i, length) {
+          return '<link rel="stylesheet" href="' + filePath.replace('app/', '') + '"/>';
+        }
+      }))
+    .pipe(plugins.inject(
+      gulp.src([src + 'modules/**/*.js'], { read: true }).pipe(plugins.angularFilesort()), {
+        name:'modules',
+        addRootSlash: false,
+        transform: function (filePath, file, i, length) {
+          return '<script src="' + filePath.replace('app/', '') + '"></script>';
+        }
+      }))
+    .pipe(plugins.inject(
+      gulp.src([src + 'js/**/*.js'], { read: false }), {
+        name:'main',
+        addRootSlash: false,
+        transform: function (filePath, file, i, length) {
+          return '<script src="' + filePath.replace('app/', '') + '"></script>';
+        }
+      }))
+    .pipe(gulp.dest(src));
+});
 // Default Task
-gulp.task('build', ['index', 'scripts', 'vendor-scripts', 'vendor-css', 'my-files'])
-gulp.task('default', ['scripts', 'images', 'watch', 'webserver']);
-gulp.task('browse', ['build', 'watch', 'webserver-build']);
+gulp.task('build', ['index', 'scripts', 'vendor-scripts-build', 'vendor-css-build', 'my-files'])
+gulp.task('browse', ['build', 'webserver-build']);
+gulp.task('dev', ['vendor-css','vendor-scripts','index-dev', 'webserver']);
